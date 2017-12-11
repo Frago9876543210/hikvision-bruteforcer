647303c5941299c5b3c17a0296052eedf627b398
Frago9876543210 <al.volf.av@gmail.com>
2017-12-11 18:33:11 +0300

    Oops. Fogot this class

---
+++ web-cam-bruteforcer/Dahua.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace web_cam_bruteforcer
+{
+    public class Dahua
+    {
+        [DllImport("DahuaHCNetSDK.dll")]
+        public static extern bool CLIENT_Init(string host, int unknown = 0);
+
+        [DllImport("DahuaHCNetSDK.dll")]
+        public static extern int CLIENT_Login(string host, int port, string login, string password, object unknown = null);
+    }
+}
